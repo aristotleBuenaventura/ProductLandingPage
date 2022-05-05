2f77d82636f9c81a11f923470004b798310214c6
aristoleBuenaventura <aristotlecortezbuenaventura@gmail.com>
2022-05-05 16:07:42 +0800

    Added Download button. Separated the CSS. Put some JS functions- Content inside each feature is still not created

---
+++ index.js
@@ -0,0 +1,20 @@
+function notAvailableYet() {
+    alert("This feature is not available yet! Stay tuned");
+  }
+
+
+function downloadMessage() {
+    document.querySelector("#download_message").innerHTML = "Test!"
+}
+
+function noAddsMessage() {
+    document.querySelector("#no-ads_message").innerHTML = "Test!"
+}
+
+function playButtonMessage() {
+    document.querySelector("#play-button_message").innerHTML = "Test!"
+}
+
+function unlimitedMessage() {
+    document.querySelector("#unlimited_message").innerHTML = "Test!"
+}
