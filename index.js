cd987d84236eebdc0eb946b83345a54db7480cb3
aristoleBuenaventura <aristotlecortezbuenaventura@gmail.com>
2022-05-05 19:57:50 +0800

    Changed the content of each features

--- index.js
+++ index.js
@@ -4,17 +4,17 @@ function notAvailableYet() {
 
 
 function downloadMessage() {
-    document.querySelector("#download_message").innerHTML = "Everything you need to do to get started with Spotify registers for a free account. The service was original, by invitation, only to help the company manage the demand for their product. Now you can begin using Spotify with your Facebook username, thanks to a recent collaboration with Facebook. Spotify also allows users to create their own playlists which can be found conveniently by using the search feature."
+    document.querySelector("#download_message").innerHTML = "To get started with Melofy, all you have to do is create a free account. The program was unique and only available by invitation to assist the corporation in managing demand for their product. Thanks to a recent agreement with Facebook, you may now use Melofy using your Facebook username. Melofy also allows users to make their own playlists, which they can easily find using the search option."
 }
 
 function noAddsMessage() {
-    document.querySelector("#no-ads_message").innerHTML = "Listening to Spotify can be something social. If you have connected Spotify to your Facebook account, then your friends and family can see what you’re listening to and you would also be able to share your favourite songs with them."
+    document.querySelector("#no-ads_message").innerHTML = "Listening to Melofy can be a social experience. If you connect Melofy to your Facebook account, your friends and family will be able to see what you're listening to and share your favorite songs with them."
 }
 
 function playButtonMessage() {
-    document.querySelector("#play-button_message").innerHTML = "Switching to Spotify Premium lets you save up to 10,000 songs on 5 different devices each. You can save any of the albums or playlists to listen offline. Besides, you don’t see any advertising at all with Premium. You can also listen to any song on your phones any time the same way you listen to it on your laptop. Since you’re no longer limited to shuffling, you can skip tracks, access Radio, and see what the app has to offer."
+    document.querySelector("#play-button_message").innerHTML = "By upgrading to Melofy Premium, you can save up to 10,000 songs across five different devices. Any of the albums or playlists can be downloaded to listen to later. Furthermore, Premium does not include any advertising. You can listen to any song on your phone at any time, just like you can on your desktop. You can skip tracks, listen to Radio, and explore the app now that you're not limited to shuffle."
 }
 
 function unlimitedMessage() {
-    document.querySelector("#unlimited_message").innerHTML = "One of Spotify ‘s features is its higher compatibility. Along with the Spotify web app available on all platforms, both Spotify Free users and Paid subscription holders can access it using the Spotify apps for Windows, iOS, iPhone, Linux, MeeGo, Open Pandora, OS X, Roku, S60, Smart TV, Sonos, PlayStation 3 & 4, and Android devices."
+    document.querySelector("#unlimited_message").innerHTML = "Melofy's increased compatibility is one of its advantages. Melofy Free and Paid subscribers can utilize the Melofy apps for Windows, iOS, iPhone, Linux, MeeGo, Open Pandora, OS X, Roku, S60, Smart TV, Sonos, PlayStation 3 & 4, and Android devices, in addition to the Melofy web app, which is available on all platforms."
 }
