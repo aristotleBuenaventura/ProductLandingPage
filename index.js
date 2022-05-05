8c223904442e4bca383b74ff57bc13ae3792c522
aristoleBuenaventura <aristotlecortezbuenaventura@gmail.com>
2022-05-05 19:49:05 +0800

    Added the content for each feature, added hover effect.

--- index.js
+++ index.js
@@ -4,17 +4,17 @@ function notAvailableYet() {
 
 
 function downloadMessage() {
-    document.querySelector("#download_message").innerHTML = "Test!"
+    document.querySelector("#download_message").innerHTML = "Everything you need to do to get started with Spotify registers for a free account. The service was original, by invitation, only to help the company manage the demand for their product. Now you can begin using Spotify with your Facebook username, thanks to a recent collaboration with Facebook. Spotify also allows users to create their own playlists which can be found conveniently by using the search feature."
 }
 
 function noAddsMessage() {
-    document.querySelector("#no-ads_message").innerHTML = "Test!"
+    document.querySelector("#no-ads_message").innerHTML = "Listening to Spotify can be something social. If you have connected Spotify to your Facebook account, then your friends and family can see what you’re listening to and you would also be able to share your favourite songs with them."
 }
 
 function playButtonMessage() {
-    document.querySelector("#play-button_message").innerHTML = "Test!"
+    document.querySelector("#play-button_message").innerHTML = "Switching to Spotify Premium lets you save up to 10,000 songs on 5 different devices each. You can save any of the albums or playlists to listen offline. Besides, you don’t see any advertising at all with Premium. You can also listen to any song on your phones any time the same way you listen to it on your laptop. Since you’re no longer limited to shuffling, you can skip tracks, access Radio, and see what the app has to offer."
 }
 
 function unlimitedMessage() {
-    document.querySelector("#unlimited_message").innerHTML = "Test!"
+    document.querySelector("#unlimited_message").innerHTML = "One of Spotify ‘s features is its higher compatibility. Along with the Spotify web app available on all platforms, both Spotify Free users and Paid subscription holders can access it using the Spotify apps for Windows, iOS, iPhone, Linux, MeeGo, Open Pandora, OS X, Roku, S60, Smart TV, Sonos, PlayStation 3 & 4, and Android devices."
 }
